6586f35890175e93087f3b0e0f3590409eb79029
Guilherme-Humberto <gbreu@outlook.com>
2020-10-13 14:59:53 -0300

    Trabalhando operações e valores

---
+++ lessons/Operations/operations.ex
@@ -0,0 +1,16 @@
+# Operações aritiméticas
+
+# Com a função div() pode-se dividir dois valores
+valor = 10
+result = div(valor, 2)
+IO.puts(result)
+
+# Com a função rem() pode-se pegar o resta da divisão de dois valores
+IO.puts(rem(10, 3))
+
+# O elixir suporta notações de atalho para inserir números binários, octais e hexadecimais:
+IO.puts(0b1010)
+
+# Com a função round pode-se pegar o valor inteiro mais proximo
+valor = 10.8
+IO.puts(round(valor))
