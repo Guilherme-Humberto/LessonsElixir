46cf8848cf06c5f2b982b074b96a52c2704fabfc
Guilherme-Humberto <gbreu@outlook.com>
2020-10-15 00:44:15 -0300

    Trabalhando com enum

--- lessons/Enum/enum.ex
+++ lessons/Enum/enum.ex
@@ -1,5 +1,5 @@
 # Trabalhando com enums
-#A enumeração é o núcleo da programação funcional e é uma coisa incrivelmente útil.
+# A enumeração é o núcleo da programação funcional e é uma coisa incrivelmente útil.
 
 
 # A função all? irá analisar cada um dos valores do Enum
